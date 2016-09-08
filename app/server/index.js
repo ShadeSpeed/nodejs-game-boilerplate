aa22bd242e31756bf8c36dc3c9ee7c0c39253947
Danny Price <DannyPrice@danprice.home>
2016-09-08 23:59:56 +0100

    new file:   .gitignore 	new file:   .idea/dictionaries/DannyPrice.xml 	new file:   app/client/index.jsx 	new file:   app/server/database.js 	new file:   app/server/index.js 	new file:   package.json

---
+++ app/server/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by DannyPrice on 08/09/2016.
+ */
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', function() { client.end(); });
